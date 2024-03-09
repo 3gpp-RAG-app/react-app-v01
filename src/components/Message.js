febb8db83178996755c7f0e9e09712f608550fe0
ArunJ0 <anttijussi.niku@gmail.com>
2024-03-09 14:20:37 +0200

    Chatbot is typing function + Textinput resets to normal size when message sent

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,7 +8,7 @@ const Message = () => {
   const [serverResponse, setServerResponse] = useState([]);
   const [chatMessages, setChatMessages] = useState([]);
   const [showSourceArray, setShowSourceArray] = useState([]);
-
+  const [isChatbotTyping, setIsChatbotTyping] = useState(false);
 
   const handleToggleClick = (index) => {
     setShowSourceArray((prevArray) => {
@@ -18,21 +18,27 @@ const Message = () => {
     });
   };
 
-
   const submitToDatabase = async () => {
-    if (!userInput) {
+
+    const userMessageForAPI = userInput;
+    setUserInput('');
+    const textArea = document.getElementById("user_input");
+    textArea.style.height = `50px`;
+    
+    if (!userMessageForAPI) {
       alert("Please enter a valid input");
       return;
     }
-  
+
     try {
-      // User's message
-      const userMessage = { type: 'user', text: userInput };
+      const userMessage = { type: 'user', text: userMessageForAPI };
       setChatMessages((prevMessages) => [...prevMessages, userMessage]);
-  
-      const response = await axios.post(apiEndpoints.search, { query: userInput });
+
+      setIsChatbotTyping(true); // Set typing indicator
+
+      const response = await axios.post(apiEndpoints.search, { query: userMessageForAPI });
       setServerResponse(response.data);
-  
+
       const textMessage = {
         type: 'server',
         text: response.data.results.augmented_response,
@@ -42,15 +48,14 @@ const Message = () => {
           text: response.data.results.results_text,
         },
       };
-  
+
       setChatMessages((prevMessages) => [...prevMessages, textMessage]);
-      
     } catch (error) {
       console.error('Error:', error);
       alert("An error occurred. Please try again.");
+    } finally {
+      setIsChatbotTyping(false); // Clear typing indicator
     }
-  
-    setUserInput('');
   };
 
   useEffect(() => {
@@ -58,87 +63,86 @@ const Message = () => {
       try {
         const uid = sessionStorage.getItem('uid');
         const logs = JSON.stringify(chatMessages);
-  
+
         const formData = new FormData();
         formData.append('uid', uid);
         formData.append('logs', logs);
-  
+
         navigator.sendBeacon(apiEndpoints.logs, formData);
       } catch (error) {
         console.error('Error posting session logs:', error);
       }
     };
-  
+
     window.addEventListener('beforeunload', handleBeforeUnload);
-  
+
     return () => {
       window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, [chatMessages]);
-
 
   return (
     <div className='flex h-full flex-col space-y-4 rounded-md border'>
       <div className="basis-1/7 bg-white pl-5 pt-3 pb-3">3gpp Chat V0.1</div>
 
       <div className='h-full basis-5/7 bg-white bg-opacity-70 rounded-md m-5 overflow-auto'>
-      <div className='p-5 font-sans text-lg antialiased'>
-        {chatMessages.map((message, index) => (
-          <div key={index} className={message.type === 'user' ? 'user-message' : 'server-message'}>
-            {message.type === 'user' ? (
-              <div className=" bg-white rounded-md p-3 m-3 flex flex-row">
-                <div className='basis-6/7 pl-3'> {message.text}</div>
-              </div>
-             ) : (
-              <div className="bg-white rounded-md p-3 m-3">
-                <div>{message.text}</div>
-                <div className='pt-4 text-sm italic' onClick={() => handleToggleClick(index)} style={{ cursor: 'pointer' }}>
-                Source: {message.source.parentDoc} section {message.source.contentList}
+        <div className='p-5 font-sans text-lg antialiased'>
+          {chatMessages.map((message, index) => (
+            <div key={index} className={message.type === 'user' ? 'user-message' : 'server-message'}>
+              {message.type === 'user' ? (
+                <div className=" bg-white rounded-md p-3 m-3 flex flex-row">
+                  <div className='basis-6/7 pl-3'> {message.text}</div>
                 </div>
-                {showSourceArray[index] && (
-                  <div>
-                    {message.source.text}
+              ) : (
+                <div className="bg-white rounded-md p-3 m-3">
+                  <div>{message.text}</div>
+                  <div className='pt-4 text-sm italic' onClick={() => handleToggleClick(index)} style={{ cursor: 'pointer' }}>
+                    Source: {message.source.parentDoc} section {message.source.contentList}
                   </div>
-                )}
-              </div>
-            )}
-          </div>
-        ))}
+                  {showSourceArray[index] && (
+                    <div>
+                      {message.source.text}
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          ))}
+          {isChatbotTyping && <div className="bg-white rounded-md p-3 m-3">Chatbot is typing...</div>}
+        </div>
       </div>
+
+      <div className="basis-1/7 rounded-md m-5 flex items-center justify-between bg-white p-2">
+        <div className="flex size-full">
+          <textarea
+            type="textarea"
+            id="user_input"
+            className="flex-1 border p-2 mr-2 text-lg"
+            placeholder="Enter your text here"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            style={{ height: "50px", minHeight: "50px", maxHeight: "200px" }}
+            onInput={() => {
+              const textArea = document.getElementById("user_input");
+              if (textArea.value.trim() === "") {
+                textArea.style.height = "50px"; // Set back to base height if empty
+              } else {
+                textArea.style.height = `${Math.min(textArea.scrollHeight, 200)}px`;
+              }
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault(); // Prevent default form submission
+                submitToDatabase(); // Call your submit function
+              }
+            }}
+          />
+
+          <button onClick={submitToDatabase} className="rounded-full w-32">
+            Send
+          </button>
+        </div>
       </div>
-
-<div className="basis-1/7 rounded-md m-5 flex items-center justify-between bg-white p-2">
-  <div className="flex size-full">
-    <textarea
-      type="textarea"
-      id="user_input"
-      className="flex-1 border p-2 mr-2 text-lg"
-      placeholder="Enter your text here"
-      value={userInput}
-      onChange={(e) => setUserInput(e.target.value)}
-      style={{ height: "50px", minHeight: "50px", maxHeight: "200px" }}
-      onInput={() => {
-        const textArea = document.getElementById("user_input");
-        if (textArea.value.trim() === "") {
-          textArea.style.height = "50px"; // Set back to base height if empty
-        } else {
-          textArea.style.height = `${Math.min(textArea.scrollHeight, 200)}px`;
-        }
-      }}
-      onKeyDown={(e) => {
-        if (e.key === "Enter" && !e.shiftKey) {
-          e.preventDefault(); // Prevent default form submission
-          submitToDatabase(); // Call your submit function
-        }
-      }}
-    />
-
-    <button onClick={submitToDatabase} className="rounded-full w-32">
-      Send
-    </button>
-  </div>
-</div>
-
 
       <div className='pl-5 pb-3 text-xs'>This Alpha release of the 3gpp chat app </div>
     </div>
