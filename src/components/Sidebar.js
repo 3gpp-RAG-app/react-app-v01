06065147f555c379f84239af32fdb440c5430edc
Mufida Alakulju <mufidaalakulju@gmail.com>
2024-03-09 22:51:09 +0200

    some cleaning and pic resizing

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,7 @@ const Sidebar = () => {
     <div className="flex h-full pb-3 bg-[#487792] rounded-md">
       <div className="flex flex-col w-full ">
         <div className="flex items-center justify-center bg-sky-900 p-3 text-white rounded-t-md">
-          <Link to="/" className="text-xl font-semibold">Yak Chat</Link>
+          <Link to="/" className="text-xl font-semibold">3gpp Chat</Link>
         </div>
         <nav className="mt-2">
           <Link to="/user-guide" className="block p-2 text-xl font-medium text-center text-white hover:bg-sky-900">
