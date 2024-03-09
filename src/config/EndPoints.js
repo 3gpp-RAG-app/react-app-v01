11a26426d1c3e8af854de6350c3bba4d52e6b9c6
Mufida Alakulju <mufidaalakulju@gmail.com>
2024-03-09 23:05:21 +0200

    herokku update

--- src/config/EndPoints.js
+++ src/config/EndPoints.js
@@ -1,5 +1,4 @@
-const apiBaseUrl = 'http://127.0.0.1:5000/milvus';
-//'http://16.16.24.95:8000/milvus';
+const apiBaseUrl = 'http://13.49.21.2:8000/milvus';
 
 export const apiEndpoints = {
     search: `${apiBaseUrl}/search`,
